2b12cf0a275c226ad08d82913190ad7c89532398
ysushko <yurasushko2000@gmail.com>
2024-11-26 18:11:49 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from "./Login";
+import Timer from "./Timer";
+import {useContext, useState} from "react";
+import { CardNumProvider, CardNumContext } from "./CardNumContext";
+import Cards from "./Cards";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+
+
+    return (
+
+        <CardNumProvider>
+            <div className="App">
+
+
+                <Login/>
+                <Cards/>
+                { <Timer/>}
+
+            </div>
+        </CardNumProvider>
+    );
 }
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,50 @@
+import React, {useContext, useEffect} from 'react';
+import {useState} from 'react';
+import {CardNumContext, CardNumProvider} from "./CardNumContext";
+
+function Login() {
+    const [name,setName] = useState("");
+    const { cardNum, setCardNum } = useContext(CardNumContext);
+    const [cardNumBeforeValid, setCardNumBeforeValid ] = useState(0);
+    const [startGame, setStartGame] = useState(false);
+    const setCardNumberIfValid=(num)=>{
+        if (name!=="") {
+            if ((num > 0 && num <= 30 && num % 2 === 0)) {
+                setCardNumBeforeValid(num)
+                alert("to Start game with " + name + " totaling " + num + " cards pres start game");
+            } else {
+                //setCardNumBeforeValid(0)
+                alert("number of cards should be even (max of 30)")
+            }
+        }else{alert("input your name first ;)")}
+    }
+
+    useEffect(() => {
+        if(startGame){
+            if(cardNumBeforeValid > 0){setCardNum(cardNumBeforeValid)}
+            setStartGame(false);
+        }
+    }, [startGame]);
+
+     const iWasClicked=()=>{
+         setStartGame(true)
+     }
+
+
+    return(
+        <div>
+            {cardNum === 0 &&
+                <div>
+                    <input value={name} type="text" onChange={(e) => setName(e.target.value)}/>
+                    <input id="inputCardNum" value={cardNumBeforeValid} type="number"
+                    onChange={(e) => setCardNumberIfValid(Number(e.target.value))}/>
+                    <button id="buttonStart" onClick={iWasClicked}>Start Game</button>
+                </div>
+                }
+       </div>
+                )
+
+
+}
+
+export default Login;
